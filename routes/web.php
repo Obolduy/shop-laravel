7b1f6970ce088dee9b9b3c319e23715aeca40eba
Obolduy <ivanox01@yandex.ru>
2021-02-20 20:15:39 +0300

    routes added

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,48 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [MainController::class, 'main']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/catalog/all', [CategoryController::class, 'showall']);
+
+Route::get('/catalog/{id}', [CategoryController::class, 'showcategory']);
+
+Route::get('/catalog/{category-id}/{subcategory-id}', [CategoryController::class, 'showsubcategory']);
+
+Route::get('/catalog/{category-id}/{subcategory-id}/{lot-id}', [LotController::class, 'showlot']);
+
+Route::get('/cart', [CartController::class, 'showcart']);
+
+Route::get('/cart/add', [CartController::class, 'addtocart']);
+
+Route::get('/cart/delete', [CartController::class, 'deletefromcart']);
+
+Route::get('/cart/buy', [CartController::class, 'buy']);
+
+Route::get('/cart/buy/payment', [CartController::class, 'payform']);
+
+Route::get('/cart/buy/payment/success', [CartController::class, 'showcart']);
+
+Route::get('/profile', [UserController::class, 'showprofile']);
+
+Route::get('/profile/change', [UserController::class, 'changeprofile']);
+
+Route::get('/profile/delete', [UserController::class, 'deleteprofile']);
+
+Route::get('/profile/my-shops', [UserController::class, 'showusershops']);
+
+Route::get('/profile/my-lots', [UserController::class, 'showuserlots']);
+
+Route::get('/create-shop', [ShopController::class, 'createshop']);
+
+Route::get('/{shop}', [ShopController::class, 'showshop']);
+
+Route::get('/{shop}/change', [ShopController::class, 'changeshop']);
+
+Route::get('/{shop}/manage-lots', [ShopController::class, 'manageshoplots']);
+
+Route::get('/{shop}/delete', [ShopController::class, 'deleteshop']);
+
+Route::get('/shops/{shop-name}', [ShopController::class, 'showothershop']);
+
+Route::get('/{shop}/catalog', [CategoryController::class, 'showshoplots']);
