bb74315db741c5cd496912844c0f3f7b2bb44682
Obolduy <ivanox01@yandex.ru>
2021-03-19 21:54:23 +0300

    style changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,21 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="{{ asset('style/style.css') }}"/>
     <title>Магазинчик</title>
 </head>
 <body>
     <header>
-        @if (session('auth'))
-        <div class="logout">
-            <a href="/logout">Выйти</a>
+        <div class="cart">
+            <div class="cart-btn">
+                <form action="/cart">
+                    <button>Корзина</button>
+                </form>
+            </div>
         </div>
+        <div class="auth">
+        @if (session('auth'))
+            <div class="logout">
+                <form action="/logout">
+                    <button>Выйти</button>
+                </form>
+            </div>
             <div class="profile">
-                <a href="/profile">{{ Auth::user()->login }}</a>
+                <form action="/profile">
+                    <button>{{ Auth::user()->login }}</button>
+                </form>
             </div>
         @else
-        <div class="auth">
             <div class="reg-btn">
                 <form action="/registration">
                     <button>Зарегистрироваться</button>
@@ -28,17 +39,7 @@
                     <button>Войти</button>
                 </form>
             </div>
-        </div>
         @endif
-        <div class="title">
-            Магазин «Magazine»
-        </div>
-        <div class="cart">
-            <div class="cart-btn">
-                <form action="/cart">
-                    <button>Корзина</button>
-                </form>
-            </div>
         </div>
     </header>
     
